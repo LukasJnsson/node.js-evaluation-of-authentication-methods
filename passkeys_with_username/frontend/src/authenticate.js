3fe2cc61bcc19bdf74afb094e450b9616dbf178a
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2024-02-28 12:07:26 +0100

    Changed scenarios

---
+++ passkeys_with_username/frontend/src/authenticate.js
@@ -0,0 +1,110 @@
+
+import { encodeBufferToSafeBase64Url, decodeBase64UrlString, verifyWebAuthAuthentication } from "./passkey";
+
+
+export default {
+  /**
+   * Authenticate by sign up
+   * @param {String} username username
+   * @returns {Object} user
+   */
+  async signUp(username) {
+
+    // Fetch users
+    const response = await fetch('http://localhost:3001/api/v1/users');
+    const users = await response.json();
+  
+    // Validate if username is unique
+    const userExists = users.find(user => user.username === username);
+      if (userExists) {
+        throw new Error("Invalid credentials!");
+    };
+  
+    // Fetch challange
+    const getChallange = await fetch('http://localhost:3001/api/v1/challanges');
+    const challengeArray = await getChallange.json();
+    const challenge = new Uint8Array(challengeArray);
+
+    /**
+     * Web Authentication API (WebAuth) (create)
+     * https://developer.mozilla.org/en-US/docs/Web/API/Web_Authentication_API
+     */
+    const publicKeyCredential = await navigator.credentials.create({
+      publicKey: {
+        rp: { name: "node.js-end-to-end-encrypted-platform" },
+        user: {
+          id: crypto.getRandomValues(new Uint8Array(32)),
+          name: username,
+          displayName: username,
+        },
+        pubKeyCredParams: [
+          /**
+           * Elliptic Curve Digital Signature Algorithm (ES256)
+           */
+          { alg: -7, type: 'public-key' },
+          /**
+           * RSA Signature Algorithm (RS256) (based on RSA cryptography)
+           */
+          { alg: -257, type: 'public-key' },
+        ],
+        challenge,
+      }
+    });
+  
+    const data = await fetch('http://localhost:3001/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        credential_id: publicKeyCredential.id,
+        username,
+        public_key: encodeBufferToSafeBase64Url(publicKeyCredential.response.getPublicKey())
+      })
+    });
+    const userData = await data.json();
+    return userData;
+  },
+
+  /**
+   * Authenticate by sign in
+   * @param {String} userCred user credentials
+   * @returns {Object} id and username of user
+   */
+  async signIn(userCred) {
+    const generateChallenge = await fetch('http://localhost:3001/api/v1/challanges');
+    const challengeArray = await generateChallenge.json();
+    const challenge = new Uint8Array(challengeArray);
+  
+    /**
+     * Web Authentication API (WebAuth) (get)
+     * https://developer.mozilla.org/en-US/docs/Web/API/Web_Authentication_API
+     */
+    const base64 = userCred.replace(/-/g, '+').replace(/_/g, '/');
+    const id = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+    
+    const publicKeyCredentialRequestOptions = {
+      challenge: challenge,
+      allowCredentials: [{
+        id: id,
+        type: "public-key",
+      }],
+    };
+    const publicKeyCredential = await navigator.credentials.get({ publicKey: publicKeyCredentialRequestOptions });
+  
+    // Fetch user by id
+    const response = await fetch(`http://localhost:3001/api/v1/users/${publicKeyCredential.id}`);
+    const user = await response.json();
+  
+    // Verify authentication
+    await verifyWebAuthAuthentication(publicKeyCredential, {
+      publicKey: decodeBase64UrlString(user.public_key),
+      challenge
+    });
+  
+    return {
+      username: user.username,
+      credential_id: user.credential_id
+    };
+  }
+};
